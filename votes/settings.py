3c301ea74f131c016091cadad6b08d518cf275b2
Rishats <sultanov-rishat@inbox.ru>
2017-09-17 21:15:12 +0600

    Install our module votes-leader. (part 2)

--- votes/settings.py
+++ votes/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'votes_leader.apps.VotesLeaderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
